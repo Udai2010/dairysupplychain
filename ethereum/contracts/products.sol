e6a5248a51ecabb50782ebda2a297e55ace42024
Udai2010 <udai.prasad.5@gmail.com>
2022-06-10 00:14:26 +0530

    All the contracts have been Integrated

--- ethereum/contracts/products.sol
+++ ethereum/contracts/products.sol
@@ -1,299 +1,193 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.9;
 
-contract Product {
-    struct ProductDetails {
-        uint256 ID;
+contract Stakeholders {
+    struct StakeholderDetails{
+        uint id;
         string name;
-        uint256 quantity;
+        uint timestamp;
+        uint quantitySupply;
+        string location;
+        uint temperature;
+    }
+
+    mapping(uint => StakeholderDetails) private stakeholder_details;
+
+    uint private stakeholderCount;
+
+    function addStakeholder(string memory _name, uint supply, string memory _location, uint _temperature) internal {
+        StakeholderDetails storage newStakeholder = stakeholder_details[++stakeholderCount];
+        newStakeholder.id = stakeholderCount;
+        newStakeholder.name = _name;
+        newStakeholder.timestamp = block.timestamp;
+        newStakeholder.quantitySupply = supply;
+        newStakeholder.location = _location;
+        newStakeholder.temperature = _temperature;
+    }
+
+    function getStakeholderDetails(uint stakeholderID) internal view returns (StakeholderDetails memory) {
+        return stakeholder_details[stakeholderID];
+    }
+
+    function getNumberOfStakeholders() internal view returns (uint) {
+        return stakeholderCount;
+    }
+    
+}
+
+contract Processes{
+    struct Process{
+        uint ID;
         string description;
+        bool active;
+    }
+
+    mapping(uint => Process) process_details;
+
+    constructor(){
+        process_details[1]=Process(1,"Raw Material",false);
+        process_details[2]=Process(2,"Pasteurization",false);
+        process_details[3]=Process(3,"Out of Delivery",false);
+    }
+
+    modifier isValidProcessID(uint _id){
+        require(_id > 0);
+        _;
+    }
+
+    function isActive(uint _id) internal view isValidProcessID(_id) returns(bool) {
+        return process_details[_id].active;
+    }
+
+    function changeState(uint _id) internal isValidProcessID(_id) {
+        process_details[_id].active = true;
+    }
+}
+
+contract Product is Stakeholders,Processes {
+    struct ProductDetails{
+        uint ID;
+        string name;
+        uint quantity;
         string globalID;
-        uint256[] traceProducts;
-        uint256[] temperatureProducts;
-        uint256 traceCount;
-        uint256 temperatureCount;
+        uint[] traceProducts;
+        uint[] temperatureProducts;
+        uint traceCount;
+        uint temperatureCount;
+        uint stakeholderCount;
+        StakeholderDetails[] stakeholderList;    
+        mapping(uint => Process) processList;    
+        uint currStatus;
     }
 
     // mapping the struct w.r.t productID
-    mapping(uint256 => ProductDetails) private product_details;
-
-    struct Trace {
-        uint256 traceID;
-        uint256 productID;
-        string location;
-        uint256 timestamp;
-        address maker; // who is updating the location
-    }
-
-    mapping(uint256 => Trace) private traces;
-
-    struct Temperature {
-        uint256 temperatureID;
-        uint256 productID;
-        uint256 celsius;
-        string temp_owner;
-        uint256 timestamp;
-        address maker; // who is updating the location
-    }
-
-    mapping(uint256 => Temperature) private temperatures;
+    mapping(uint => ProductDetails) private product_details; 
 
     // It is necessary to have the count since we cannot loop the mapped elements
-    uint256 productCount;
-    uint256 traceCount;
-    uint256 temperatureCount;
+    uint productCount;
 
     address stakeholder;
     address admin;
 
-    // events (i.e. to notify the stakeholder)
-    bool trigger;
-    bool delivery;
-    bool recieved;
-
     // while deploying the contract the admin must specify who is the stakeholder
-    constructor(address stakeholder_address) {
+    constructor(address _stakeholder) {
         admin = msg.sender;
-        stakeholder = stakeholder_address;
-        trigger = false;
-        delivery = false;
-        recieved = false;
+        stakeholder = _stakeholder;
     }
 
     // Modifiers required for our contract
 
     // To ensure whether the user is an admin
-    modifier isAdmin() {
+    modifier isAdmin {
         require(msg.sender == admin);
         _;
     }
 
     // To ensure whether the user is a stakeholder
-    modifier isStakeholder() {
+    modifier isStakeholder {
         require(msg.sender == stakeholder);
         _;
     }
 
     // Either admin or stakeholder
-    modifier isAdminOrStakeholder() {
+    modifier isAdminOrStakeholder {
         require(msg.sender == admin || msg.sender == stakeholder);
         _;
     }
 
     // To ensure the given product is valid
-    modifier isValidProductID(uint256 _productID) {
+    modifier isValidProductID(uint _productID) {
         require(_productID > 0 && _productID <= productCount);
         _;
     }
 
-    modifier isValidTraceID(uint256 _traceID) {
-        require(_traceID > 0 && _traceID <= traceCount);
+    modifier isSupplyAcceptable(uint productID, uint supply){
+        require(supply <= product_details[productID].quantity);
         _;
     }
-
-    modifier isValidTemperatureID(uint256 _temperatureID) {
-        require(_temperatureID > 0 && _temperatureID <= temperatureCount);
-        _;
-    }
-
-    // events
-    event triggeredEvent();
-
-    event updatedEvent();
-
-    event reqEvent(uint256 indexed productID);
-
+    
     // functions required for our contract
     // product
-    function addProduct(
-        string memory _name,
-        uint256 _quantity,
-        string memory _description,
-        string memory _globalID
-    ) public isAdminOrStakeholder {
+    function addProduct(string memory _name, uint _quantity, string memory _globalID) public isAdminOrStakeholder {
         ProductDetails storage newProduct = product_details[++productCount];
         newProduct.ID = productCount;
         newProduct.name = _name;
         newProduct.quantity = _quantity;
-        newProduct.description = _description;
         newProduct.globalID = _globalID;
+        newProduct.stakeholderCount = 0;
+        
+        // To add the process to each new products
+        for(uint id=1; id<=3; id++){
+            newProduct.processList[id] = process_details[id];
+        }
+
+        newProduct.currStatus = 1;
+        newProduct.processList[newProduct.currStatus].active = true;
+        
     }
 
-    function getProduct(uint256 productID)
-        public
-        isAdminOrStakeholder
-        isValidProductID(productID)
-        returns (
-            uint256,
-            string memory,
-            uint256,
-            string memory,
-            string memory
-        )
-    {
+
+    function addStakeholderToProduct(uint productID,string memory _name, uint supply, string memory _location, uint _temperature) public isAdminOrStakeholder isValidProductID(productID) isSupplyAcceptable(productID,supply){
+        addStakeholder(_name,supply,_location,_temperature);
+        uint stakeholderId = getNumberOfStakeholders();
         ProductDetails storage existingProduct = product_details[productID];
-        emit reqEvent(productID);
-        return (
-            existingProduct.ID,
-            existingProduct.name,
-            existingProduct.quantity,
-            existingProduct.description,
-            existingProduct.globalID
-        );
+        existingProduct.stakeholderList.push(getStakeholderDetails(stakeholderId));
+        existingProduct.quantity -= supply;
+
+        if(existingProduct.quantity == 0){
+            existingProduct.currStatus++;
+            existingProduct.processList[existingProduct.currStatus].active = true;
+        }
+
     }
 
-    function updateProductDescription(
-        uint256 productID,
-        string memory newDescription
-    ) public isAdminOrStakeholder isValidProductID(productID) {
+    function getProductStakeholder(uint productID) private view isAdminOrStakeholder isValidProductID(productID) returns (StakeholderDetails[] memory){
         ProductDetails storage existingProduct = product_details[productID];
-        existingProduct.description = newDescription;
-        emit updatedEvent();
+        return existingProduct.stakeholderList;
     }
 
-    function getNumberOfProducts() public view returns (uint256) {
+    function getProductQuantity(uint productID) public view isAdminOrStakeholder isValidProductID(productID) returns(uint){
+        return product_details[productID].quantity;
+    }
+
+    function getProduct(uint productID) public view isAdminOrStakeholder isValidProductID(productID) returns(uint, string memory,  uint, string memory, StakeholderDetails[] memory){
+        ProductDetails storage existingProduct = product_details[productID];
+        return (existingProduct.ID, existingProduct.name, existingProduct.quantity, existingProduct.globalID,getProductStakeholder(productID));
+    }
+
+    function getNumberOfProducts() private view returns(uint) {
         return productCount;
     }
 
-    function getProductGlobalID(uint256 productID)
-        public
-        view
-        isValidProductID(productID)
-        returns (string memory)
-    {
-        return product_details[productID].globalID;
+    function getCurrStatus(uint productID) public view isValidProductID(productID) returns(uint) {
+        ProductDetails storage existingProduct = product_details[productID];
+        return existingProduct.currStatus;
     }
 
-    // Trace
-    function addTrace(uint256 _productID, string memory _location)
-        public
-        isAdminOrStakeholder
-        isValidProductID(_productID)
-    {
-        Trace storage newTraceProduct = traces[++traceCount];
-        newTraceProduct.traceID = traceCount;
-        newTraceProduct.productID = _productID;
-        newTraceProduct.location = _location;
-        newTraceProduct.timestamp = block.timestamp;
-        newTraceProduct.maker = msg.sender;
-        product_details[_productID].traceProducts.push(traceCount);
-        product_details[_productID].traceCount++;
-
-        emit updatedEvent();
+    function finishPasteurization(uint productID) public isAdminOrStakeholder isValidProductID(productID) {
+        ProductDetails storage existingProduct = product_details[productID];
+        existingProduct.currStatus++;
+        existingProduct.processList[existingProduct.currStatus].active = true;
     }
-
-    function getNumberOfTraces() public view returns (uint256) {
-        return traceCount;
-    }
-
-    function getTrace(uint256 traceID)
-        public
-        view
-        isAdminOrStakeholder
-        isValidTraceID(traceID)
-        returns (
-            uint256,
-            uint256,
-            string memory,
-            uint256,
-            address
-        )
-    {
-        Trace storage existingTrace = traces[traceID];
-        return (
-            existingTrace.traceID,
-            existingTrace.productID,
-            existingTrace.location,
-            existingTrace.timestamp,
-            existingTrace.maker
-        );
-    }
-
-    function getNumberOfTracesProduct(uint256 productID)
-        public
-        view
-        isAdminOrStakeholder
-        isValidProductID(productID)
-        returns (uint256)
-    {
-        return product_details[productID].traceCount;
-    }
-
-    function getTracesProduct(uint256 productID)
-        public
-        view
-        isAdminOrStakeholder
-        isValidProductID(productID)
-        returns (uint256[] memory)
-    {
-        return product_details[productID].traceProducts;
-    }
-
-    // Temperature
-    function addTemperature(uint256 _productID, uint256 _celsius)
-        public
-        isAdminOrStakeholder
-        isValidProductID(_productID)
-    {
-        Temperature storage newTemperature = temperatures[++temperatureCount];
-        newTemperature.temperatureID = temperatureCount;
-        newTemperature.productID = _productID;
-        newTemperature.celsius = _celsius;
-        newTemperature.timestamp = block.timestamp;
-        newTemperature.maker = msg.sender;
-
-        emit updatedEvent();
-    }
-
-    function getNumberOfTemperatures() public view returns (uint256) {
-        return temperatureCount;
-    }
-
-    function getTemperature(uint256 temperatureID)
-        public
-        view
-        isAdminOrStakeholder
-        isValidTemperatureID(temperatureID)
-        returns (
-            uint256,
-            uint256,
-            uint256,
-            uint256,
-            address
-        )
-    {
-        Temperature storage existingTemperature = temperatures[temperatureID];
-        return (
-            existingTemperature.temperatureID,
-            existingTemperature.productID,
-            existingTemperature.celsius,
-            existingTemperature.timestamp,
-            existingTemperature.maker
-        );
-    }
-
-    function getNumberOfTemperaturesProduct(uint256 productID)
-        public
-        view
-        isAdminOrStakeholder
-        isValidProductID(productID)
-        returns (uint256)
-    {
-        return product_details[productID].temperatureCount;
-    }
-
-    function getTemperaturesProduct(uint256 productID)
-        public
-        view
-        isAdminOrStakeholder
-        isValidProductID(productID)
-        returns (uint256[] memory)
-    {
-        return product_details[productID].temperatureProducts;
-    }
-
-    function triggerContract() public {
-        trigger = true;
-        emit triggeredEvent();
-    }
+    
 }
